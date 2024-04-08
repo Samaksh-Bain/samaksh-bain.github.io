4991f105be56227a301f6d3b6c42c1e226d29b68
Samaksh-Bain <samaksh.jhanjhari@bain.com>
2024-04-08 09:21:02 +0530

    Changed file name

--- earthquakeWDC.js
+++ earthquakeWDC.js
@@ -1,5 +1,31 @@
 (function () {
     var myConnector = tableau.makeConnector();
+    
+    myConnector.getSchema = function (schemaCallback) {
+        var cols = [{
+            id: "id",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "mag",
+            alias: "magnitude",
+            dataType: tableau.dataTypeEnum.float
+        }, {
+            id: "title",
+            alias: "title",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "location",
+            dataType: tableau.dataTypeEnum.geometry
+        }];
+    
+        var tableSchema = {
+            id: "earthquakeFeed",
+            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+            columns: cols
+        };
+    
+        schemaCallback([tableSchema]);
+    };
 
    
     myConnector.getData = function(table, doneCallback) {
@@ -61,36 +87,11 @@
        
     };
 
-    myConnector.getSchema = function (schemaCallback) {
-        var cols = [{
-            id: "id",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "mag",
-            alias: "magnitude",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "title",
-            alias: "title",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "location",
-            dataType: tableau.dataTypeEnum.geometry
-        }];
-    
-        var tableSchema = {
-            id: "earthquakeFeed",
-            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
-            columns: cols
-        };
-    
-        schemaCallback([tableSchema]);
-    };
-
+   
     tableau.registerConnector(myConnector);
     $(document).ready(function () {
         $("#submitButton").click(function () {
-            tableau.connectionName = "USGS Earthquake Feed";
+            tableau.connectionName = "Survey feed";
             tableau.submit();
         });
     });
